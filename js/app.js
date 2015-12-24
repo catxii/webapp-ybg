6bca83e0aa38e3ef5107aacbdaea4f163fc82534
catxii <catxii@qq.com>
2015-12-24 10:41:36 +0800

    修改提交

--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ function login_model($scope, $http) {
 		//登陆请求
 	var ajaxUrl = publicAjaxUrl + "Mobile/Driver/";
 	$scope.submit = function() {
+		loadingModel();
 		$.ajax({
 			url: ajaxUrl + "login.html",
 			type: "post",
@@ -71,7 +72,8 @@ function login_model($scope, $http) {
 					localStorage.setItem("userphone", data.mobile);
 					//设置保存司机城市
 					localStorage.setItem("usercity", data.city);
-					window.location.href = "#/driver/start";
+					window.location.href = "#/me/index";
+					getOrderCount(); //获取订单数量
 				} else {
 					alert("不支持本地存储")
 				}
@@ -93,6 +95,14 @@ var myData = {
 	}
 };
 //获取订单数量
+var startNumber,onSendNumber,completeNumber;
+function getJsonLength(jsondata){
+	var length = 0;
+	for(var i in jsondata){
+		length++;
+	}
+	return length;
+}
 function getOrderCount(){
 	//获取未配送的订单数量
 	var startOrderURL = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=waitSend";
@@ -101,9 +111,7 @@ function getOrderCount(){
 		type:"get",
 		data:{"id": userid},
 		success:function(data){
-			statusList = data;
-			var startNumber =  JSON.parse(data).length;
-			return startNumber;
+			startNumber =  getJsonLength(JSON.parse(data));
 		}
 	})
 	//获取配送中的订单数量
@@ -113,9 +121,7 @@ function getOrderCount(){
 		type:"get",
 		data:{"id": userid},
 		success:function(data){
-			statusList = data;
-			var onSendNumber =  JSON.parse(data).length;
-			return onSendNumber;
+			onSendNumber =   getJsonLength(JSON.parse(data));
 		}
 	})
 	//获取已签收的订单数量
@@ -125,24 +131,36 @@ function getOrderCount(){
 		type:"get",
 		data:{"id": userid},
 		success:function(data){
-			statusList = data;
-			var completeNumber =  JSON.parse(data).length;
-			return completeNumber;
+			completeNumber =   getJsonLength(JSON.parse(data));
 		}
 	})
+	return getOrderCount;
+}
+//加载loading动画
+function loadingModel(){
+	$(document).on("ajaxStart",function (e){
+		$.showPreloader();
+	}).on('ajaxStop',function (e) {
+        $.hidePreloader();
+    });
 }
 
 //配送开始页面
 function status_index_model($scope, $http) {
+	loadingModel();
 	getOrderCount();
+	$scope.startnum = startNumber;
+	$scope.deliverynum = onSendNumber;
+	$scope.completenum = completeNumber;
 	var orderURL = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=waitSend";
 	$http.get(orderURL, myData).success(function(data) {
-			$scope.statusList = data;
-		})
-		//获取当前订单ID
+		$scope.statusList = data;
+	})
+	//获取当前订单ID
 	$scope.view_order_detail = function() {
 		startOrderId = this.list.id
 	}
+	//开始配送按钮
 	$scope.start_delivery = function() {
 		startOrderId = this.list.id;
 		$.modal({
@@ -191,10 +209,15 @@ function status_index_model($scope, $http) {
 
 //配送中页面
 function status_delivery_model($scope, $http) {
+	loadingModel();
+	getOrderCount();
+	$scope.startnum = startNumber;
+	$scope.deliverynum = onSendNumber;
+	$scope.completenum = completeNumber;
 	//	alert(myData.params.id)
 	var orderURL = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=onSend";
 	$http.get(orderURL, myData).success(function(data) {
-			$scope.statusList = data;
+			$scope.statusList = [data];
 		})
 		//获取当前订单ID
 	$scope.view_order_detail = function() {
@@ -204,27 +227,27 @@ function status_delivery_model($scope, $http) {
 	$scope.editCarryPay = function($scope2){
 		var that = this;
 		$.prompt('请输入金额(元)', '建议修改搬运费',
-                function (value) {
-        		    var carryData = {
-						params: {
-							"dispatch_id": that.list.id,
-							"money": value,
-							"driver_id": userid
-						}
-					};
-					var carryUrl = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=deliveryFee";
-					$http.get(carryUrl, carryData).success(function(data) {
-						if( data.success == 1 ){
-        					     $.alert('搬运费修改为' + value +'元,等用户确认后生效');
-								 that.list.carryRecord = {"status":0};
-        					 }else if( data.success == 0 ){
-        						 $.alert('搬运费输入错误或者不能为空');
-        					 }
-					})
-                }	
-            );
-
+            function (value) {
+    		    var carryData = {
+					params: {
+						"dispatch_id": that.list.id,
+						"money": value,
+						"driver_id": userid
+					}
+				};
+				var carryUrl = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=deliveryFee";
+				$http.get(carryUrl, carryData).success(function(data) {
+					if( data.success == 1 ){
+					     $.alert('搬运费修改为' + value +'元,等用户确认后生效');
+						 that.list.carryRecord = {"status":0};
+					 }else if( data.success == 0 ){
+						 $.alert('搬运费输入错误或者不能为空');
+					 }
+				})
+            }	
+        );
 	}
+
 	//客户签收
 	$scope.confirmtake = function(){
 		var that = this;
@@ -245,20 +268,70 @@ function status_delivery_model($scope, $http) {
 			
 		})
 	}
+
+	//查询过滤
 }
 
 //配送完成页面
 function status_complete_model($scope, $http) {
-	var orderURL = publicAjaxUrl + "/index.php?m=Mobile&c=Driver&a=completeSend";
+	loadingModel();
+	getOrderCount();
+	$scope.startnum = startNumber;
+	$scope.deliverynum = onSendNumber;
+	$scope.completenum = completeNumber;
+	var orderURL = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=completeSend";
 	$http.get(orderURL, myData).success(function(data) {
 			$scope.statusList = data;
-		})
-		//获取当前订单ID
+	})
+	//获取当前订单ID
 	$scope.view_order_detail = function() {
 		startOrderId = this.list.id
 	}
 	$scope.uploadImg = function(){
+		var that = this;
+		var imgUpUrl = publicAjaxUrl+"index.php?m=Mobile&c=Driver&a=downphotograph";
+		var myImgData = {
+			params: {
+				"dispatch_id": that.statusList.id,
+				"driver_id": userid,
+				"site_info_id":"", //工地地址
+				"media_id":""//图片地址
+			}
+		}
+		
+		// 调取相册照片
+		function galleryImg() { 
+			// 从相册中选择图片
+			// outSet("从相册中选择图片:");
+		    plus.gallery.pick( function(path){
+		        //showImg( path );
+		        //createItem(path);
+		        // $.alert(path);
+		         var orderURL = publicAjaxUrl+"index.php?m=Mobile&c=Driver&a=uploadFile";
+		        //var orderURL = "http://demo.dcloud.net.cn/helloh5/uploader/upload.php";
+				$.showPreloader("开始上传");
+				var task=plus.uploader.createUpload(orderURL,
+					{method:"POST"},
+					function(t,status){ //上传完成
+						if(status==200){
+							$.hidePreloader();
+							$.toast("上传成功");
+							$.alert(t.responseText);
+						}else{
+							alert(JSON.stringify(t));
+							$.hidePreloader();
+							alert("上传失败："+status);
+						}
+					}
+				);
+				task.addFile(path,{key:"云包公图片"});
+				task.start();
 
+		    }, function ( e ) {
+		    	$.alert( "取消选择图片" );
+		    }, {filter:"image"} );
+		}
+		galleryImg();
 	}
 }
 
@@ -329,7 +402,6 @@ function status_detail_model($scope, $http) {
 				"driver_id": userid
 			}
 		};
-		alert(takeData.params.dispatch_id+":"+takeData.params.driver_id)
 		var carryUrl = publicAjaxUrl + "index.php?m=Mobile&c=Driver&a=customerSign";
 		$http.get(carryUrl, takeData).success(function(data){
 			alert(JSON.stringify(data));
